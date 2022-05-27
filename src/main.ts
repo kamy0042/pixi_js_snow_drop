6915ec8745628ced630ec1c61069e8274fc49879
kamy18112 <kamahori.dev@gmail.com>
2022-05-27 16:51:15 +0900

    雪のアニメーションを実装

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,43 @@
 import './style.css'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+import * as PIXI from "pixi.js"
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const createApp = () => {
+    const WIDTH = 640;
+    const HEIGHT = 480;
+
+    const app = new PIXI.Application( {width:WIDTH, height:HEIGHT, backgroundColor: 0x1C1E36 } );
+    const blurFilter = new PIXI.filters.BlurFilter();
+    blurFilter.blur = 2;
+    app.stage.filters = [blurFilter];
+
+    document.body.appendChild(app.view);
+
+    const SNOW_LIMIT = 600;
+    const snows = [];
+
+    for (let i = 0; i < SNOW_LIMIT; i++) {
+        const scale = (Math.floor(Math.random() * 6) + 5) / 10;
+        snows.push(new PIXI.Graphics().beginFill(0xFFFFFF,0.7).drawCircle(0,0,7).endFill());
+        snows[i].position.x = Math.random() * WIDTH;
+        snows[i].position.y = Math.random() * HEIGHT;
+        snows[i].scale.x = scale;
+        snows[i].scale.y = scale;
+        app.stage.addChild(snows[i]);
+    }
+
+    app.ticker.add(() => {
+        for (let i = 0; i < SNOW_LIMIT; i++) {
+            const scale = snows[i].scale.x;
+            snows[i].position.x += (Math.random() - 0.5) * 1 * scale;
+            snows[i].position.y += (scale * scale) + 0.1;
+            if (snows[i].position.y > HEIGHT) {
+                snows[i].position.y = -10;
+            }
+        }
+    } );
+
+    return app;
+}
+
+const app = createApp();
